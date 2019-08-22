6b782bdddbc1e6281c73d10a493db0a25c2181da
Malthe <malthe@tedib.dk>
2019-08-22 13:31:05 +0200

    v2.1

---
+++ content/js/form.js
@@ -0,0 +1,45 @@
+// Form validation. //
+// Form submit button //
+const submit = document.getElementById('submit');
+// Inputs //
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const city = document.getElementById('city');
+const post = document.getElementById('post');
+const adress = document.getElementById('adress');
+
+
+submit.onclick = () => {
+    if (name.value.length <= 0 ) {
+    name.style.border = "2px solid red";
+} else {
+    name.style.border = "unset";
+    name.placeholder = 'Husk at udfylde her'
+}
+    if (city.value.length <= 0 ) {
+    city.style.border = "2px solid red";
+    city.placeholder = 'Husk at udfylde her'
+} else {
+    city.style.border = "unset";
+}
+if (adress.value.length <= 0 ) {
+    adress.style.border = "2px solid red";
+    adress.placeholder = 'Husk at udfylde her'
+} else {
+    adress.style.border = "unset";
+}
+    if (email.value.length <= 0 ) {
+    email.style.border = "2px solid red";
+    email.placeholder = 'Husk at udfylde her'
+} else {
+    email.style.border = "unset";
+}
+
+if (post.value.length < 4 ) {
+    post.style.border = "2px solid red";
+    post.placeholder = 'Husk at udfylde her'
+} else {
+    post.style.border = "unset";
+}
+
+}
